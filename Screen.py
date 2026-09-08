db76de2ccc4b2aad135cd2ff058969bb003ec069
almaperry <almaperub@gmail.com>
2026-09-08 16:27:37 +0300

    added main

--- Screen.py
+++ Screen.py
@@ -34,3 +34,24 @@ def game():
     game_field.draw(screen)
 
 
+
+def draw_lose_message():
+    draw_message(consts.LOSE_MESSAGE, consts.LOSE_FONT_SIZE,
+                 consts.LOSE_COLOR, consts.LOSE_LOCATION)
+    draw_message("press space to play again", 30,
+                 consts.LOSE_COLOR, (300, 600))
+
+
+def draw_win_message():
+    draw_message(consts.WIN_MESSAGE, consts.WIN_FONT_SIZE,
+                 consts.WIN_COLOR, consts.WIN_LOCATION)
+    draw_message("press space to play again", 30,
+                 consts.LOSE_COLOR, (300, 400))
+
+
+def draw_message(message, font_size, color, location):
+    font = pygame.font.SysFont(consts.FONT_NAME, font_size)
+    text_img = font.render(message, True, color)
+    screen.blit(text_img, location)
+
+
--- game_field.py
+++ game_field.py
@@ -54,30 +54,21 @@ def main_bomb ():
     for i in range(consts.MINES_COUNT):
         y=random.randint(0, 24)
         x=random.randint(0,49)
-        while [x,y] in randon_place and (BOARD_GAME[y][x] == "SOLDIER" or BOARD_GAME[y][x] == "FLAG") :
+        while [x,y] in randon_place and (BOARD_GAME[y][x] == "SOLDIER" or BOARD_GAME[y][x] == "FLAG") and x+2 >= len(BOARD_GAME[y])-1  :
             y = random.randint(0,24)
             x = random.randint(0,49)
         BOARD_GAME[y][x] = "MAIN"
         BOARD_GAME[y][x+1] = "MAIN"
         BOARD_GAME[y][x+2] = "MAIN"
+    return randon_place
 
 
 
-
-
+"""
 def soldier_place():
     for i in range(consts.BOARD_ROWS):
         for j in range(consts.BOARD_COLS):
             if BOARD_GAME[i][j] == "SOLDIER":
                 return ((i*20,j*20))
-    return ((0,0))
+    return ((0,0))"""
 
-#
-##
-
-# def flag_place():
-#     for i in range(consts.BOARD_ROWS):
-#         for j in range(consts.BOARD_COLS):
-#             if BOARD_GAME[i][j] == "FLAG":
-#                 return (i * 20) - 300, (j * 20) - 300
-#     return 0,0
