fb5b2420ba67b8afef55fe6f0ff052114aa019a9
jbt <shilatkolani90@gmail.com>
2026-09-08 15:23:19 +0300

    gffg

--- Screen.py
+++ Screen.py
@@ -20,7 +20,7 @@ def background():
 
 def flag_display():
     small_img = pygame.transform.scale(img_flag, (100, 100))
-    screen.blit(small_img, game_field.flag_place)
+    screen.blit(small_img, (0,0))
 
 
 def bushs():
