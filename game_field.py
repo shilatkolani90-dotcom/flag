e32583c64d824747c0a0ac2c2813bda6b8c2e8c9
almaperry <almaperub@gmail.com>
2026-09-08 11:19:43 +0300

    added main

--- game_field.py
+++ game_field.py
@@ -1,19 +1,49 @@
+from random import random
+
 from flag import consts
 
-board_game = []
-
+BOARD_GAME = []
+TEMP =[]
 
 #empty board
-for i in range(consts.BOARD_ROWS):
-    for j in range(consts.BOARD_COLS):
+def empty_board ():
+    for i in range(consts.BOARD_ROWS):
+        for j in range(consts.BOARD_COLS):
+            TEMP.append(consts.EMPTY_PLACE)
+        BOARD_GAME.append(TEMP)
+        tamp = []
+    soldier()
+    flag()
+    #main_bomb()
+
+    return BOARD_GAME
 
 
+def soldier ():
+    for i in range(consts.SOLDIER_ROWS):
+        for j in range(consts.SOLDIER_COLS):
+            BOARD_GAME[i][j] = consts.SOLDIER
 
 
+def flag():
+    for i in range(consts.BOARD_ROWS,0):
+        for j in range(consts.BOARD_COLS,0):
+            BOARD_GAME[i][j]= consts.FLAG
 
 
+"""def main_bomb ():
+    x=0
+    y=0
+    randon_place=[]
+    for i in range(consts.MINES_COUNT):
+        y=random(0,25)
+        x=random(0,50)
+        while [x,y] in randon_place:
+            y = random(0, 25)
+            x = random(0, 50)
+        randon_place.append([x,y])
+    
+    for i in range(consts.MINES_COUNT):
+        for
 
-
-
-
-
+"""
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-
+import game_field
 
 def main ():
-
+    board_game = game_field.empty_board()
+    print(board_game)
 
 
 
