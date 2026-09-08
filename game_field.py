83985c6c9cea21ffbe9961f36aa04aa68fdbdf61
almaperry <almaperub@gmail.com>
2026-09-08 14:33:23 +0300

    added main

--- game_field.py
+++ game_field.py
@@ -1,5 +1,4 @@
-from random import random
-
+import random
 from flag import consts
 
 BOARD_GAME = []
@@ -14,7 +13,7 @@ def empty_board ():
         tamp = []
     soldier()
     flag()
-    #main_bomb()
+    main_bomb()
 
     return BOARD_GAME
 
@@ -25,38 +24,58 @@ def soldier ():
             BOARD_GAME[i][j] = consts.SOLDIER
 
 
+def add_soldier ():
+    for i in range(consts.FLAG_ROWS):
+        for j in range(consts.FLAG_COLS):
+            BOARD_GAME[i][j] = consts.FLAG
+
+
+
+
 def flag():
-    for i in range(consts.BOARD_ROWS,0):
-        for j in range(consts.BOARD_COLS,0):
-            BOARD_GAME[i][j]= consts.FLAG
-
-#
-# def main_bomb ():
-#     x=0
-#     y=0
-#     randon_place=[]
-#     for i in range(consts.MINES_COUNT):
-#         y=random(0,25)
-#         x=random(0,50)
-#
-#
+    for i in range(consts.BOARD_ROWS):
+        BOARD_GAME[i].reverse()
+    BOARD_GAME.reverse()
+    add_soldier()
+    for i in range(consts.BOARD_ROWS):
+        BOARD_GAME[i].reverse()
+    BOARD_GAME.reverse()
 
 
 
-    """  
-        while [x,y] in randon_place or on soldier or on flag:  
-            y = random(0, 25)
-            x = random(0, 50)
-        randon_place.append([x,y])
-    
+
+
+
+def main_bomb ():
+    x=0
+    y=0
+    randon_place=[]
     for i in range(consts.MINES_COUNT):
-        for"""
+        y=random.randint(0, 24)
+        x=random.randint(0,49)
+        while [x,y] in randon_place and (BOARD_GAME[y][x] == "SOLDIER" or BOARD_GAME[y][x] == "FLAG") :
+            y = random.randint(0,24)
+            x = random.randint(0,49)
+        BOARD_GAME[y][x] = "MAIN"
+        BOARD_GAME[y][x+1] = "MAIN"
+        BOARD_GAME[y][x+2] = "MAIN"
+
+
+
 
 
 def soldier_place():
     for i in range(consts.BOARD_ROWS):
         for j in range(consts.BOARD_COLS):
             if BOARD_GAME[i][j] == "SOLDIER":
-                return ((i,j))
+                return ((i*20,j*20))
     return ((0,0))
 
+
+
+def flag_place():
+    for i in range(consts.BOARD_ROWS):
+        for j in range(consts.BOARD_COLS):
+            if BOARD_GAME[i][j] == "FLAG":
+                return ((i*20,j*20))
+    return ((0,0))
